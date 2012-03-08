f8546f1eaa9a6c3516f684128f7aec4de6f2dc4e
trodoss@hotmail.com <trodoss@hotmail.com@639ee549-df5b-8796-c8cd-9c80bcaf7485>
2012-03-08 21:06:28 +0000

    commit of initial PoofyAdvendure code to repository.

---
+++ VideoGameShield/poofy_adventure/poofy.cpp
@@ -0,0 +1,157 @@
+#include <avr/pgmspace.h>
+#include <TVout.h>
+#include "poofy.h"
+#include "poofy_bitmap.h"
+#include "bitmap_funcs.h"
+#include "level.h"
+
+extern TVout TV;
+
+#define MAX_VELOCITY 4
+#define STEP_LENGTH 2
+#define SIZEOF_POOFY_RECORD 10
+
+char x_velocity = 0;
+char y_velocity = 0;
+char x = 8;
+char y = 8;
+char facing = FACING_LEFT;
+char frame = 0;
+char count = 0;
+char state = POOFY_STANDING;
+
+void poofy_draw()
+{
+     char tile_adjust;
+     
+     //handle delay for changing x velocity, y velocity, or when jumping (special case)
+     if ((count >= MAX_VELOCITY - x_velocity) || (y_velocity > 0) || (state == POOFY_JUMPING))
+     {
+         //erasebitmap(x, y, poofy_bitmap);
+         eraseBitmapRect(x, y, poofy_bitmap);
+         frame++; 
+         count = 0;
+     
+	 //handle x axis
+         if (x_velocity > 0)
+         {
+            if (facing == FACING_RIGHT)
+	    {
+             //if we have hit someting moving right, adjust accordingly
+		if (level_check_move_v(x + x_velocity + 8, y, tile_adjust) > 0)
+                {
+                   x = (tile_adjust * 8) - 9;
+                   x_velocity = 0;
+                //otherwise, keep moving right 
+                } else {		
+		   x += x_velocity;
+                   x_velocity--;
+                }		
+	   } else {
+               if (level_check_move_v(x - x_velocity, y, tile_adjust) > 0)
+               {
+                   x = ((tile_adjust + 1) * 8) + 1;
+                   x_velocity = 0;
+               } else {
+		   x -= x_velocity;
+                   x_velocity--;
+               }	    
+	    }
+         }
+	 
+	 //handle y axis
+	 if (y_velocity > 0)
+	 {
+	    //moving up
+	    if (state == POOFY_JUMPING)
+	    {
+		   if (level_check_move_h(x, y-y_velocity, tile_adjust) > 0)
+		   {
+		      y = ((tile_adjust + 1) * 8) + 1;
+		      y_velocity = 1;
+	              //poofy starting to freefall
+		     state = POOFY_FALLING;
+		   } else {
+		      y -= y_velocity;
+		      y_velocity--;
+		   }
+	    } else {
+		   if (level_check_move_h(x, y+y_velocity+8, tile_adjust) > 0)
+		   {
+   		      y = (tile_adjust * 8) - 9;
+		      y_velocity = 0;
+		      //poofy standing on ground
+		      state = POOFY_STANDING;
+		   } else {
+		      y += y_velocity;
+			  if (y_velocity <= MAX_VELOCITY) y_velocity++;
+		   }
+	   } 
+	 } else {
+	    //test for gravity pulling down (if nothing is below)
+		if (level_check_move_h(x, y+9, tile_adjust) == 0)
+		{
+		   state = POOFY_FALLING;
+		   y_velocity = 1;
+		}
+	 }
+	 
+	 //determine animation frames to show
+	 if (y_velocity > 0)
+	 {
+            if (facing == FACING_LEFT) frame = POOFY_JUMP_LEFT;
+            if (facing == FACING_RIGHT) frame = POOFY_JUMP_RIGHT;
+	 } else {
+	    if (x_velocity > 0)
+            {
+	       if ((facing == FACING_LEFT) && (frame > 6)) frame = POOFY_MOVE_LEFT;
+               if ((facing == FACING_RIGHT) && (frame > 2)) frame = POOFY_MOVE_RIGHT;
+            } else {
+	        if (facing == FACING_LEFT) frame = POOFY_FACE_LEFT;
+                if (facing == FACING_RIGHT) frame = POOFY_FACE_RIGHT;
+            }
+         }
+	 
+        overlaybitmap(x, y, poofy_bitmap + ( frame * SIZEOF_POOFY_RECORD));
+     } else {
+       count++;
+     }
+}
+
+void poofy_move(char new_facing)
+{
+  if (state < POOFY_FALLING)
+  {
+     //if we are still moving in the same direction, accelerate
+     if (new_facing == facing)
+     {
+       if (x_velocity <= MAX_VELOCITY) x_velocity++;
+     } else {
+       facing = new_facing;
+       if (facing == FACING_LEFT)
+       {
+          frame = POOFY_MOVE_LEFT;
+       } else {
+          frame = POOFY_MOVE_RIGHT;
+       }
+       x_velocity = 1;
+     }
+   }
+}
+
+void poofy_jump()
+{
+  //make sure that we are in a state where jumping is possible
+  //(can't jump if already jumping/falling)
+  //if (y_velocity == 0)
+  if (state < POOFY_JUMPING)
+  {
+    y_velocity = MAX_VELOCITY;
+    state = POOFY_JUMPING;
+  }  
+}
+
+char poofy_getState()
+{
+  return state;
+}
---
+++ VideoGameShield/poofy_adventure/poofy_adventure.ino
@@ -0,0 +1,153 @@
+/*
+
+   An Adventure Game for the Arduino/Video Game Shield 
+
+   2011 - trodoss
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+ */
+ 
+#include <stdlib.h> 
+#include <i2cmaster.h>
+#include <nunchuck.h>
+#include <TVout.h>
+#include <font4x6.h>
+
+#define SCREENWIDTH     128
+#define SCREENHEIGHT    96
+
+#define PAUSE_BETWEEN_ACTIONS 50
+
+#define GAME_TITLE   0
+#define GAME_PLAYING 1
+#define GAME_PAUSED  2
+#define GAME_WON     3
+#define GAME_OVER    4
+
+#include "sound.h"
+#include "logo.h"
+#include "level.h"
+#include "poofy.h"
+
+TVout TV;
+Nunchuck player1;
+char game_state;
+//pollserial pserial;
+
+void setup()
+{
+  TV.begin(_NTSC, SCREENWIDTH, SCREENHEIGHT);
+  TV.select_font(font4x6);
+  //debug
+  //TV.set_hbi_hook(pserial.begin(9600));  
+  
+  randomSeed(analogRead(0));
+  
+  player1.begin(0);
+  start_title();
+}
+
+void loop()
+{  
+  static long next_action = 0;
+  switch (game_state)
+  {
+    case GAME_TITLE:
+	case GAME_OVER:
+	case GAME_WON:
+       if (TV.millis() >= next_action)
+       {
+         player1.update();
+		 //wait for a button to be pressed to continue
+         if ((player1.button_c()) || (player1.button_z()))
+         {
+		   
+		   //check to see what we are doing when we start the game
+		   if (game_state > GAME_TITLE)
+		   {
+		     if (game_state == GAME_WON)
+			 {
+				//to do: win game
+			 } else {
+				//to do: handle returning to the title
+			 }
+		   }
+		   
+		   game_state = GAME_PLAYING;
+		   start_game();
+         }
+		         
+         next_action = TV.millis() + PAUSE_BETWEEN_ACTIONS; 
+       }	
+	   sound_update();
+	   break;
+	   
+	case GAME_PLAYING:
+       if (TV.millis() >= next_action)
+       {      
+         player1.update();
+       
+         if (player1.joy_right()) poofy_move(FACING_RIGHT);
+         if (player1.joy_left()) poofy_move(FACING_LEFT);
+         if (player1.button_z()) poofy_jump();
+         if (player1.button_c()) game_state = GAME_PAUSED;
+    
+         next_action = TV.millis() + PAUSE_BETWEEN_ACTIONS; 
+	
+         level_update();
+		 
+		 //check the poofy's state
+		 if (poofy_getState() > POOFY_JUMPING)
+		 {
+		   if (poofy_getState() == POOFY_DEAD)
+		   {
+		     start_game_over();
+		   } else {
+		     start_game_won();
+		   }
+		 }
+       }
+       sound_update();
+       break;
+       
+   case GAME_PAUSED:
+       if (TV.millis() >= next_action)
+       {
+         player1.update();
+		 
+         if (player1.button_c()) game_state = GAME_PLAYING;
+         
+         next_action = TV.millis() + PAUSE_BETWEEN_ACTIONS; 
+       }
+       break;
+  }
+}
+
+void start_game()
+{
+  game_state = GAME_PLAYING;  
+  level_draw();
+}
+
+void start_title()
+{
+  game_state = GAME_TITLE;
+  logo_draw();
+}
+
+void start_game_over()
+{
+}
+
+void start_game_won()
+{
+}
