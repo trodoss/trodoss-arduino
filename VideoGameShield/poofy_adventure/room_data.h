ec8e3b6a53ea57f693da4c64aa74044d4dabd4e7
trodoss@hotmail.com <trodoss@hotmail.com@639ee549-df5b-8796-c8cd-9c80bcaf7485>
2012-04-30 17:52:00 +0000

    Additional changed files

---
+++ VideoGameShield/poofy_adventure/room_data.h
@@ -0,0 +1,17 @@
+#define ELEMENT_COUNT 7
+#define ELEMENT_SIZE 4
+
+//room element index
+PROGMEM prog_uchar room_element_index_data[] = {
+0, 1, 2, 3, 4, 255, 5, 6};
+
+//room element data
+PROGMEM prog_uchar room_element_data[] = {
+50, 80, 23, 255,
+0, 80, 15, 0, 50, 96, 31, 255,
+50, 32, 39, 0, 0, 120, 15, 255,
+50, 16, 47, 0, 0, 104, 15, 255,
+50, 32, 55, 0, 0, 120, 31, 255,
+0, 8, 47, 0, 50, 16, 55, 0, 50, 112, 31, 255,
+0, 48, 31, 0, 50, 80, 23, 255,
+};
