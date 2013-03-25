acf8b84cc90298566fc6b331c0113c150dd54fe8
trodoss@hotmail.com <trodoss@hotmail.com@639ee549-df5b-8796-c8cd-9c80bcaf7485>
2013-03-25 20:44:49 +0000

    Adding Gamby version of Elventure to the repository.

---
+++ trunk/Gamby/GambyElventure/GambyElventure.ino
@@ -0,0 +1,176 @@
+/*                                   __    
+                                   /___/
+                                   / /
+   /-------------/              __/_/__
+     |  |-------/       //===//__<*>_ //===//
+     |  .----/                 / / /
+     |  .---/                 / / /
+     |  |------/             / / /
+   /----------/ L + V + E + N + T + U + R + E *=-
+   An Adventure Game for the Gamby Game Shield 
+   v3.1
+
+   2013 - trodoss
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+ */
+ 
+#include <Gamby.h>
+#include <avr/pgmspace.h>
+
+#include "map.h"
+#include "room.h"
+#include "elf.h"
+#include "display.h"
+#include "logo.h"
+#include "sound.h"
+
+#define SCREENWIDTH     96
+#define SCREENHEIGHT    64
+
+#define PAUSE_BETWEEN_ACTIONS 100
+
+#define GAME_TITLE   0
+#define GAME_PLAYING 1
+#define GAME_WON     3
+#define GAME_OVER    4
+
+GambyGraphicsMode gamby;
+char game_state;
+
+void setup()
+{
+  
+  randomSeed(analogRead(0));
+  
+  start_title();
+}
+
+void loop()
+{  
+  static long next_action = 0;
+  byte lastInputs;
+  switch (game_state)
+  {
+    case GAME_TITLE:
+	case GAME_OVER:
+	case GAME_WON:
+       if (millis() >= next_action)
+       {
+         gamby.readInputs();
+         lastInputs = gamby.inputs;
+         
+		 //wait for a button to be pressed to continue
+         if (lastInputs == BUTTON_2)
+         {
+		   
+		   //check to see what we are doing when we start the game
+		   if (game_state > GAME_TITLE)
+		   {
+		     if (game_state == GAME_WON)
+			 {
+			   //if the game has been won, then reset the inventory of the elf
+			   resetElf(true);
+			 } else {
+			   //if the game was just over (died) then do not reset the inventory
+		       resetElf(false);
+			 }
+		   }
+		   
+		   game_state = GAME_PLAYING;
+		   start_game();
+         }
+		         
+         next_action = millis() + PAUSE_BETWEEN_ACTIONS; 
+       }	
+	   update_sound();
+	   break;
+	   
+	case GAME_PLAYING:
+       if (millis() >= next_action)
+       {    
+         gamby.readInputs();
+         lastInputs = gamby.inputs;
+         
+         if (lastInputs == DPAD_UP) moveElf(FACING_UP);
+         if (lastInputs == DPAD_DOWN) moveElf(FACING_DOWN);
+         if (lastInputs == DPAD_RIGHT) moveElf(FACING_RIGHT);
+         if (lastInputs == DPAD_LEFT) moveElf(FACING_LEFT);
+         if (lastInputs == BUTTON_2) throwSword();
+    
+         next_action = millis() + PAUSE_BETWEEN_ACTIONS; 
+	
+         handleRoomElements();
+		 
+		 //check the elf's state
+		 if (getElfState() != ELFSTATE_PLAYING)
+		 {
+		   if (getElfState() == ELFSTATE_DEAD)
+		   {
+		     start_game_over();
+		   } else {
+		     start_game_won();
+		   }
+		 }
+       }
+       gamby.update();
+       update_sound();
+       break;
+  }
+}
+
+void start_game()
+{
+  //clear the contents of the screen
+  gamby.clearScreen();
+  gamby.update();
+  
+  //start the game
+  setMapRoom(0);
+  showElf();
+  updateDisplay(getElf());
+  play_song(0);
+  game_state = GAME_PLAYING;  
+}
+
+void start_title()
+{
+  //clear the contents of the screen
+  gamby.clearScreen();
+  gamby.update();
+  
+  game_state = GAME_TITLE;
+  drawLogo(0);
+  play_song(4);
+}
+
+void start_game_over()
+{
+  //clear the contents of the screen
+  gamby.clearScreen();
+  gamby.update();
+  
+  game_state = GAME_OVER;
+  drawLogo(4);
+  play_song_once(2);
+}
+
+void start_game_won()
+{
+  //clear the contents of the screen
+  gamby.clearScreen();
+  gamby.update();
+  
+  game_state = GAME_WON;
+  drawLogo(8);
+  play_song(3);
+}
---
+++ trunk/Gamby/GambyElventure/monster.cpp
@@ -0,0 +1,122 @@
+#include <avr/pgmspace.h>
+#include <Gamby.h>
+#include "monster.h"
+#include "map.h"
+#include "map_bitmap.h"
+#include "monster_bitmap.h"
+#include "room.h"
+
+extern GambyGraphicsMode gamby;
+
+#define STEP_LENGTH  4
+#define SIZEOF_MONSTER_RECORD 10
+
+char changeMonsterDirection()
+{
+  return ((random() % 4) + STATE_MOVE_UP);
+}
+
+RoomElement moveMonster(RoomElement element)
+{
+  //draw new monster bitmap
+  if (element.state > STATE_HIDDEN)
+  {
+    //erase the old monster image (using blank map tile)
+    gamby.drawSprite(element.x, element.y, map_bitmap);
+    gamby.drawSprite(element.x, element.y + 8, map_bitmap);
+  
+    element.step++;
+    if (element.step > 2) element.step = 1;
+   
+    switch (element.state)
+    {
+       case STATE_VISIBLE:
+	    element.state = changeMonsterDirection();
+		break;
+	 
+       case STATE_MOVE_UP:
+           element.state = STATE_VISIBLE;
+           if (element.y > 4)
+           {
+             if (checkMapRoomMove(element.x, element.y - 4) == 0)
+             {
+                if (checkMapRoomMove(element.x + 4, element.y - 4) == 0)
+                {
+                  element.y -= STEP_LENGTH;
+                   element.state = STATE_MOVE_UP;
+                }
+             }
+           }
+	   break;
+	   
+	  case STATE_MOVE_DOWN:
+           element.state = STATE_VISIBLE;
+           if (element.y < 48)
+           {
+             if (checkMapRoomMove(element.x, element.y + 16) == 0)
+             {
+                if (checkMapRoomMove(element.x + 4, element.y + 16) == 0)
+                {
+                  element.y += STEP_LENGTH;
+                  element.state = STATE_MOVE_DOWN;
+                }
+             } 
+           }
+	   break;	   
+
+	  case STATE_MOVE_LEFT:
+           element.state = STATE_VISIBLE;
+           if (element.x > 4)
+           {
+             if (checkMapRoomMove(element.x - 4, element.y) == 0)
+             {
+                if (checkMapRoomMove(element.x - 4, element.y + 12) == 0)
+                {
+                    element.x -= STEP_LENGTH;
+                    element.state = STATE_MOVE_LEFT;
+                }
+             }
+           }
+	   break;	   
+	   
+	  case STATE_MOVE_RIGHT:
+           element.state = STATE_VISIBLE;
+           if (element.x < 80)
+           {
+             if (checkMapRoomMove(element.x + 12, element.y) == 0)
+             {
+                if (checkMapRoomMove(element.x + 12, element.y + 12) == 0)
+                {
+                  element.x += STEP_LENGTH;
+                  element.state = STATE_MOVE_RIGHT;
+                }
+             }
+           }
+	   break;		      
+    }
+
+    gamby.drawSprite(element.x, element.y, monster_bitmap, element.type );
+    gamby.drawSprite(element.x, element.y+8, monster_bitmap, (element.type + element.step)); 
+	
+	//decrement counter, if active
+	if (element.counter > 0) element.counter--;
+  }
+  return element;
+}
+
+RoomElement hitMonster(RoomElement element)
+{
+  //erase the old monster image (using blank map tile)
+  gamby.drawSprite(element.x, element.y, map_bitmap);
+  gamby.drawSprite(element.x, element.y + 8, map_bitmap);
+  
+  element.state = STATE_HIDDEN;
+  
+  //add heart element
+  addRoomElement(ITEM_HEART, element.x, element.y, STATE_VISIBLE, COUNTER_START);
+  
+  return element;
+}
+
+
+
