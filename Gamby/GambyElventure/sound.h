99f4b031a6fc16c294896a63ba15932c5c1b4407
trodoss@hotmail.com <trodoss@hotmail.com@639ee549-df5b-8796-c8cd-9c80bcaf7485>
2013-03-25 20:45:49 +0000

    Adding Gamby version of Elventure to the repository.

---
+++ Gamby/GambyElventure/sound.h
@@ -0,0 +1,11 @@
+#ifndef __SOUND__
+#define __SOUND__
+
+#include <avr/pgmspace.h>
+
+void play_song(char song);
+void play_song_once(char song);
+void update_sound();
+void play_sfx(char song);
+
+#endif __SOUND__
