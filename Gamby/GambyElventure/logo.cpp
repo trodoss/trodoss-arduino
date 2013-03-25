12a0756e76417289e6630c2ecbaf8cb893991b7c
trodoss@hotmail.com <trodoss@hotmail.com@639ee549-df5b-8796-c8cd-9c80bcaf7485>
2013-03-25 20:44:49 +0000

    Adding Gamby version of Elventure to the repository.

---
+++ Gamby/GambyElventure/logo.cpp
@@ -0,0 +1,21 @@
+#include <avr/pgmspace.h>
+#include <Gamby.h>
+#include "logo_bitmap.h"
+extern GambyGraphicsMode gamby;
+
+#define SIZEOF_LOGO_RECORD 10
+
+void drawLogo(char start)
+{
+  //draw hearts
+  char x_offset = 32;
+  for (char i=start; i < (start+4); i++)
+  {
+      //draw logo element
+	  gamby.setPos(x_offset, 2);
+      gamby.drawIcon(logo_bitmap, i);
+   
+    x_offset += 8;
+  }   
+}
+
